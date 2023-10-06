db88ffe63e59be9f40e398e89c55f43c0a27d9bd
erik-ferreira <erikdesouzagf@gmail.com>
2023-10-06 12:01:20 -0300

    initial routes with fastify

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-import { createServer } from "node:http"
+// import { createServer } from "node:http"
 
-const server = createServer((request, response) => {
-  response.write("Hello world")
+// const server = createServer((request, response) => {
+//   response.write("Hello world")
 
-  return response.end()
+//   return response.end()
+// })
+
+// server.listen(3333)
+
+import { fastify } from "fastify"
+
+const server = fastify()
+
+server.get("/", (request, response) => {
+  return "Hello World"
 })
 
-server.listen(3333)
+server.get("/hello", (request, response) => {
+  return "Hello Erik"
+})
+
+server.get("/node", (request, response) => {
+  return "Hello Node"
+})
+
+server.listen({
+  port: 3333,
+})
