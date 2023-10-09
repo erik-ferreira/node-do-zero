7494bf412566d76be260052f6b7377dd7b0890a6
erik-ferreira <erikdesouzagf@gmail.com>
2023-10-09 16:08:37 -0300

    create crud with neon

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 import { fastify } from "fastify"
-import { DatabaseMemory } from "./database-memory.js"
+// import { DatabaseMemory } from "./database-memory.js"
+import { DatabasePostgress } from "./database-postgres.js"
 
 const server = fastify()
-const database = new DatabaseMemory()
+// const database = new DatabaseMemory()
+const database = new DatabasePostgress()
 
-server.get("/videos", (request) => {
+server.get("/videos", async (request) => {
   const search = request.query.search
 
-  const videos = database.list(search)
+  const videos = await database.list(search)
 
   return videos
 })
 
-server.post("/videos", (request, reply) => {
+server.post("/videos", async (request, reply) => {
   const { title, description, duration } = request.body
 
-  database.create({
+  await database.create({
     title,
     description,
     duration,
@@ -24,11 +26,11 @@ server.post("/videos", (request, reply) => {
   return reply.status(201).send()
 })
 
-server.put("/videos/:id", (request, reply) => {
+server.put("/videos/:id", async (request, reply) => {
   const videoId = request.params.id
   const { title, description, duration } = request.body
 
-  database.update(videoId, {
+  await database.update(videoId, {
     title,
     description,
     duration,
@@ -37,10 +39,10 @@ server.put("/videos/:id", (request, reply) => {
   return reply.status(204).send()
 })
 
-server.delete("/videos/:id", (request, reply) => {
+server.delete("/videos/:id", async (request, reply) => {
   const videoId = request.params.id
 
-  database.delete(videoId)
+  await database.delete(videoId)
 
   return reply.status(204).send()
 })
